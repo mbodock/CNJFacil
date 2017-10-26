3bfc764676c3d16344e6e88e2bd614120916a074
lramosduarte@gmail.com <Leonardo Ramos Duarte>
2017-10-26 09:55:57 -0200

    Adicona cabeçalho de encoding ao arquivo

--- cnjfacil/extrator.py
+++ cnjfacil/extrator.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 import re
 
 from .exceptions import CNJIncorrigivel, CNJPontuacaoIncorreta
--- cnjfacil/testes/extrator.py
+++ cnjfacil/testes/extrator.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 from unittest import TestCase
 
 from cnjfacil.extrator import ExtratorCNJ
