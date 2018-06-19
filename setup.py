5419f53adb8a2e1ed2a348fa3efa4065e4ad42fb
Marcus Vinicius (Mbodock) <mbodock@gmail.com>
2018-06-19 11:23:12 -0300

    Corrige suport ao python2 no setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='CNJFacil',
-    version='0.1.4',
+    version='0.1.5',
     author='Marcus Bodock',
     author_email='mbodock@gmail.com',
     packages=['cnjfacil'],
@@ -19,9 +19,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
